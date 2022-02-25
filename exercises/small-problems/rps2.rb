e6e5f3c34608f615914978bcca1f0e554e2a070f
Joshua Briley <josh@thebrileys.com>
2022-02-25 12:18:39 -0500

    Updates to RPS after review.

--- exercises/small-problems/rps2.rb
+++ exercises/small-problems/rps2.rb
@@ -25,33 +25,15 @@ CHOICES = {
   }
 }
 
-VALID_GAME_TITLES = []
-VALID_GAME_SHORTCUTS = CHOICES.values.map { |game| game['shortcut'] }
+def valid_choice?(choice)
+  CHOICES.keys.include?(choice) ||
+    CHOICES.values.map { |value| value['shortcut'] }.include?(choice)
+end
 
-prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
-prompt("First to 3 wins!")
-prompt("Would you like to read the instructions for the game?")
-
-def separator
+def visual_separator
   puts(" ")
   puts("=" * 80)
   puts(" ")
-end
-
-instructions_answer = gets.chomp.delete(' ')
-if instructions_answer.downcase.start_with?('y')
-  puts `clear`
-  prompt("GAME INSTRUCTIONS:")
-  INSTRUCTIONS = CHOICES.map do |title, game|
-    "=> #{title.capitalize} beats #{game['beats'].join(' and ')}.
-    Choose #{title.capitalize} by typing '#{title}' or '#{game['shortcut']}'."
-  end
-  puts INSTRUCTIONS
-  separator
-end
-
-CHOICES.each do |key, _|
-  VALID_GAME_TITLES.push(key.to_s)
 end
 
 def win?(first, second)
@@ -71,16 +53,34 @@ end
 player_win_count = 0
 computer_win_count = 0
 round = 1
+user_quit = false
+
+prompt("Welcome to #{CHOICES.keys.to_a.join(', ').capitalize}!")
+prompt("First to 3 wins!")
+
+prompt("Would you like to read the instructions for the game? (y)")
+instructions_answer = gets.chomp.delete(' ')
+if instructions_answer.downcase.start_with?('y')
+  puts `clear`
+  prompt("GAME INSTRUCTIONS:")
+  INSTRUCTIONS = CHOICES.map do |title, game|
+    "=> #{title.capitalize} beats #{game['beats'].join(' and ')}.
+    Choose #{title.capitalize} by typing '#{title}' or '#{game['shortcut']}'."
+  end
+  puts INSTRUCTIONS
+end
+
+visual_separator
 
 loop do
   prompt("STARTING ROUND #{round}")
   choice = ''
 
   loop do
-    prompt("Choose one: #{VALID_GAME_TITLES.join(', ')}")
+    prompt("Choose one: #{CHOICES.keys.to_a.join(', ')}")
     choice = gets.chomp.downcase().delete(' ')
 
-    if VALID_GAME_TITLES.include?(choice) || VALID_GAME_SHORTCUTS.include?(choice)
+    if valid_choice?(choice)
       CHOICES.each do |key, value|
         if value['shortcut'] == choice
           choice = key
@@ -92,9 +92,9 @@ loop do
     end
   end
 
-  computer_choice = VALID_GAME_TITLES.sample
+  computer_choice = CHOICES.keys.sample
 
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+  prompt("You chose #{choice}. Computer chose #{computer_choice}")
 
   display_results(choice, computer_choice)
 
@@ -110,13 +110,14 @@ loop do
     prompt("Continue playing? (y or n)")
     answer = gets.chomp.delete(' ')
     if answer.downcase.start_with?('y')
-      separator
+      visual_separator
       prompt("Round #{round} sore:
         => You: #{player_win_count}
         => Computer: #{computer_win_count}")
       round += 1
-      separator
+      visual_separator
     else
+      user_quit = true
       break
     end
   end
@@ -124,12 +125,16 @@ end
 
 puts `clear`
 
-if player_win_count > computer_win_count
-  prompt("You won the game!")
-else
-  prompt("Computer won the game.")
-end
+if !user_quit
+  if player_win_count > computer_win_count
+    prompt("You won the game!")
+  else
+    prompt("Computer won the game.")
+  end
 
-prompt("Player Score: #{player_win_count}")
-prompt("Computer Score: #{computer_win_count}")
-prompt("Thank you for playing. Good bye!")
+  prompt("Player Score: #{player_win_count}")
+  prompt("Computer Score: #{computer_win_count}")
+  prompt("Thank you for playing. Good bye!")
+else
+  prompt("The computer accepts your forfeiture and claims the victory.")
+end
